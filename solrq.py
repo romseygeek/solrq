86a910c6abd033bb94a3738f8b92c6770d9110d6
Alan Woodward <alan.woodward@romseysoftware.co.uk>
2012-07-16 14:37:58 +0100

    Add --explain parameter; format output half-decently

--- solrq.py
+++ solrq.py
@@ -19,6 +19,7 @@ Output options:
 
 Debugging options:
     -v, --verbose                         verbose output
+    -x, --explain                         explain scores
 """
 
 import requests
@@ -31,23 +32,38 @@ VERSION = "0.0.1"
 def build_http_query(options, query):
     url = options['--url'] or "http://%(--host)s:%(--port)s/%(--path)s" % options
     url += "/select"
-    params = { 'q' : query, 'wt' : 'json', 'fl' : options['--fields'] }
+    params = { 'q' : query, 'wt' : 'json' }
+    fl = options['--fields']
+    if 'id' not in fl.split(','): 
+        fl += ",id"
+    params['fl'] = fl
     if options['--filter']:
         params['fq'] = options['--filter']
     if options['--phrase']:
         params['pf'] = options['--phrase']
     if options['--boost']:
         params['bq'] = options['--boost']
+    if options['--explain']:
+        params['debugQuery'] = "on"
     return url, params
 
 def format_results(options, solrresponse):
     json = solrresponse.json
     if options['--count']:
         return json['response']['numFound']
-    return [ format_result(options, doc, solrresponse) for doc in json['response']['docs'] ]
+    return [ format_result(options, doc, json) for doc in json['response']['docs'] ]
 
-def format_result(options, doc, solrresponse):
-    return doc
+def format_result(options, doc, json):
+    result = doc
+    if options['--explain']:
+        result['explain'] = json['debug']['explain'][doc['id']]
+    if 'id' not in options['--fields']:
+        del result['id']
+    return result
+
+def print_result(doc):
+    for field in doc:
+        print "%s: %s" % (field, doc[field])
 
 options = docopt(__doc__, version=VERSION)
 query = options['<query>']
@@ -55,4 +71,5 @@ url, params = build_http_query(options, query)
 response = requests.get(url, params=params)
 if options['--verbose']:
     print response.url
-pprint.pprint(format_results(options, response))
+for doc in format_results(options, response):
+    print_result(doc)
